a42f146966645cb7d208eef7b76821cd2686b716
Lamkateh <bruno.dilivio@gmail.com>
2022-03-16 13:52:28 +0100

    Cleaning code

--- src/Api.js
+++ src/Api.js
@@ -3,6 +3,7 @@ import axios from "axios"
 //require("dotenv").config();
 
 export default axios.create({
-	baseURL: process.env.REACT_APP_API_URL,
+	//baseURL: process.env.REACT_APP_API_URL,
 	//baseURL: "http://127.0.0.1:8000/api",
+	baseURL: "https://desolate-tor-38398.herokuapp.com/api",
 })
